dc77ea4bdb0fd61d206c7957ca3c2754485c2927
Mauro dos Anjos <mauriciodosanjos980@gmail.com>
2025-04-07 14:45:38 +0800

    updated by Mauro

---
+++ app/src/main/java/com/example/freelancermarketplace/Classes/ReviewCRUD.java
@@ -0,0 +1,4 @@
+package com.example.freelancermarketplace.Classes;
+
+public class ReviewCRUD {
+}
---
+++ app/src/main/java/com/example/freelancermarketplace/Classes/UserCRUD.java
@@ -0,0 +1,33 @@
+package com.example.freelancermarketplace.Classes;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class UserCRUD {
+    private DatabaseReference databaseReference;
+
+    public UserCRUD() {
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+    }
+
+    // Create or Update User
+    public void saveUser(User user) {
+        databaseReference.child(user.getUserId()).setValue(user);
+    }
+
+    // Read User by ID
+    public void getUser(String userId, final ValueEventListener listener) {
+        databaseReference.child(userId).addListenerForSingleValueEvent(listener);
+    }
+
+    // Update User
+    public void updateUser(String userId, User updatedUser) {
+        databaseReference.child(userId).setValue(updatedUser);
+    }
+
+    // Delete User
+    public void deleteUser(String userId) {
+        databaseReference.child(userId).removeValue();
+    }
+}
